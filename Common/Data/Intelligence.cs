577b66987bac596d5dfdd84c3b96a544b94ac064
CodeChrisB <c.buchberger01@gmail.com>
2023-09-19 19:18:48 +0200

    wip

---
+++ Common/Data/Intelligence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerBots.Common.Data
+{
+    public enum Intelligence
+    {
+        BadPlayer = 0,
+        Novice = 1,
+        Intermediate = 2,
+        Skilled = 3,
+        Expert = 4,
+        Master = 5,
+        Cheater = 6
+    }
+}
